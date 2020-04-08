2b042161e9d7f658b835868c12fd92a284579b52
kaizoku-619 <garssallaoui.bayrem@gmail.com>
2020-04-08 14:55:00 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+#define LED_BUILTIN 1
+#define BLINK_DELAY 1000
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(BLINK_DELAY);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(BLINK_DELAY);
+}
