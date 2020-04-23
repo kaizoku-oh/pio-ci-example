eaa5d2ede03a835c29bc471f27fe8f52e2867caa
kaizoku-619 <garssallaoui.bayrem@gmail.com>
2020-04-23 16:24:27 +0100

    blink non blocking

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
 #include <Arduino.h>
 
-#define MSG "Hello World!"
-#define LOOP_DELAY 1000
-#define SERIAL_BAUDRATE 115200
+#if defined(ESP32)
+#define LED_BUILTIN 1
+#endif // ESP32
+#define INTERVAL 500
+
+uint8_t ledState = LOW;
+unsigned long previousMillis = 0;
 
 void setup() {
-  Serial.begin(SERIAL_BAUDRATE);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  Serial.println(MSG);
-  delay(LOOP_DELAY);
+  // Check to see if it's time to process something in the loop; that is, if the difference
+  // between the current time and last time we processed something is bigger than
+  // the interval at which we want to process something.
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= INTERVAL) {
+    // Save the last time we sent data to Fireabase
+    previousMillis = currentMillis;
+    ledState = ledState == LOW ? HIGH : LOW;
+    digitalWrite(LED_BUILTIN, ledState);
+  }
 }
